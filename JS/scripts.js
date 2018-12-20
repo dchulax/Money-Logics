f6349744317ad38528638626cb42f8c3f2f35747
Joel Mackenzie <marihonjahmackenzie@gmail.com>
2018-12-20 13:17:32 +0300

    made changes

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
-  $("#sign").submit(function(){
-   var name;
-   name=$("#name").val();
-   alert(name);
-   event.preventDefault();
+  $("#submit").click(function validate(event){
+ var fullname = document.getElementById("fullname").value;
+ var password = document.getElementById("password").value;
+ if(fullname == ""){
+     alert("Please enter a User Name")
+     afterLogin.fullname.focus()
+     return false;
+ }
+ if(password == ""){
+     alert("Please enter a Password")
+     afterLogin.password.focus()
+     return false;
+ }
+ if( fullname == "mackenzie" && password == "fixed#120"){
+     alert("Well done");
+     window.location = "gameboard.html";
+     return false;
+ }
+ else{
+  alert("re-enter a correct password/fullname")
+ }
+   alert("welcome back!");
+   window.location.href="index.html"
+    event.preventDefault();
   });
 });
