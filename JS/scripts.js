dc6758a4b7c45d22d0120a7c852326608a77d71a
KevinMokoro <klosvoke1@gmail.com>
2018-12-20 20:44:12 +0300

    add all changes to js,index,about,profile.

---
+++ JS/scripts.js
@@ -0,0 +1,35 @@
+
+//function targeted (save, target) {
+//  if (target < save)
+//  {
+  //  var yy = save - target;
+  //  return yy;
+
+  //} else if (target > save)
+//  {
+    //var yy = target - save;
+  //  return yy;
+//  } else
+//  {
+  //  return target;
+//  }
+//};
+
+function xx (){
+  var xx = document.getElementById('Initial').value;
+  document.getElementById('projected').value = xx;
+  var vv = document.getElementById('Expenditure').value;
+  var kk = xx-vv;
+  document.getElementById('Saving').value = kk;
+  document.getElementById('income').value = kk;
+  var jj = document.getElementById('target').value
+  document.getElementById('target').value = jj;
+  event.preventDefault();
+}
+
+function yy (){
+  var xx = document.getElementById('Initial').value;
+  var vv = document.getElementById('Expenditure').value;
+  var kk = yy-vv
+  document.getElementById('Saving').value = kk;
+}
