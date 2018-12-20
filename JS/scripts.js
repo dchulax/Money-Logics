c18cd10155eec1dbc45110991a9d8feea2c504d2
Joel Mackenzie <marihonjahmackenzie@gmail.com>
2018-12-20 14:15:41 +0300

    made changes

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,27 +1,30 @@
-$(document).ready(function(){
-  $("#submit").click(function validate(event){
- var fullname = document.getElementById("fullname").value;
- var password = document.getElementById("password").value;
- if(fullname == ""){
-     alert("Please enter a User Name")
-     afterLogin.fullname.focus()
-     return false;
- }
- if(password == ""){
-     alert("Please enter a Password")
-     afterLogin.password.focus()
-     return false;
- }
- if( fullname == "mackenzie" && password == "fixed#120"){
-     alert("Well done");
-     window.location = "gameboard.html";
-     return false;
- }
- else{
-  alert("re-enter a correct password/fullname")
- }
-   alert("welcome back!");
-   window.location.href="index.html"
-    event.preventDefault();
-  });
+$(document).ready(function() {
+      $("#signup").click(function() {
+        var name = $(#name).val();
+        var email = $(#email).val();
+        var password = $(#password).val();
+        var conpassword = $(#conpassword).val();
+        var Occupation = $(#work).val();
+        var contact = $(#tel).val();
+        var age = $(#agez).val();
+        if (name == '' || email || Occupation || password || conpassword || contact) {
+alert("invalid insert all fields");
+} else if (!(password).match(conpassword)) {
+  alert("entered different password, please re-enter correct matching password")
+}
+$.post("register.php", {
+name1: name,
+email1: email,
+password1: password,
+Occupation1: Occupation,
+age1 = age,
+contact1 = contact,
+}, function(data) {
+if (data == 'Welcome to new experience of greatness....') {
+$("form")[0].reset();
+}
+alert(data);
+});
+}
+});
 });
