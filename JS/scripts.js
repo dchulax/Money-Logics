07e58c893b314b1b207a3fbdf7e288bfcb93008a
Joel Mackenzie <marihonjahmackenzie@gmail.com>
2018-12-21 11:43:06 +0300

    add linked index with signup html.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   $("#formic form").submit(function(event) {
 
-    var name = $('#naal();
+    var name = $('#name').val();
     var email = $('#email').val();
     var password = $('#password').val();
     var conpassword = $('#conpassword').val();
     var Occupation = $('#work').val();
     var contact = $('#tel').val();
     var age = $('#agez').val();
-    var data = [name, email, password, conpassword, Occupation, contact, age]
+    var data = [name, email, Occupation, contact, age]
     event.preventDefault()
 
     if (name == '' || email == '' || Occupation == '' || password == '' || conpassword == '' || contact == '') {
@@ -20,8 +20,8 @@ $(document).ready(function() {
       $("form")[0].reset();
     }
     else {
-      alert('Welcome to new experience of greatness..')
-      alert(data);
+      alert('Welcome to new experience of greatness...')
+      
     }
 
 
