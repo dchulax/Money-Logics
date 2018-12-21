7a1ef1702fb24080a0ce3055ffd95ec31f7cf872
Joel Mackenzie <marihonjahmackenzie@gmail.com>
2018-12-21 09:37:33 +0300

    add fuctionality on the signup form.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,30 +1,29 @@
 $(document).ready(function() {
-      $("#signup").click(function() {
-        var name = $(#name).val();
-        var email = $(#email).val();
-        var password = $(#password).val();
-        var conpassword = $(#conpassword).val();
-        var Occupation = $(#work).val();
-        var contact = $(#tel).val();
-        var age = $(#agez).val();
-        if (name == '' || email || Occupation || password || conpassword || contact) {
-alert("invalid insert all fields");
-} else if (!(password).match(conpassword)) {
-  alert("entered different password, please re-enter correct matching password")
-}
-$.post("register.php", {
-name1: name,
-email1: email,
-password1: password,
-Occupation1: Occupation,
-age1 = age,
-contact1 = contact,
-}, function(data) {
-if (data == 'Welcome to new experience of greatness....') {
-$("form")[0].reset();
-}
-alert(data);
-});
-}
-});
+  $("#formic form").submit(function(event) {
+
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+    var conpassword = $('#conpassword').val();
+    var Occupation = $('#work').val();
+    var contact = $('#tel').val();
+    var age = $('#agez').val();
+    var data = [name, email, password, conpassword, Occupation, contact, age]
+    event.preventDefault()
+
+    if (name == '' || email == '' || Occupation == '' || password == '' || conpassword == '' || contact == '') {
+      alert("invalid insert all fields");
+      $("form")[0].reset();
+    }
+    else if (password !== conpassword) {
+      alert("entered different password, please re-enter correct matching password");
+      $("form")[0].reset();
+    }
+    else {
+      alert('Welcome to new experience of greatness....')
+      alert(data);
+    }
+
+
+  });
 });
